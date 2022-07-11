f7e89a3f8cfdf87dcd6c103aaaa053d5c269b6db
dogukanyldz <dogukan1695@gmail.com>
2022-07-12 00:59:27 +0300

    serilog settings.

--- ElasticSearchWeb.Api/Program.cs
+++ ElasticSearchWeb.Api/Program.cs
@@ -33,7 +33,7 @@ namespace ElasticSearchWeb.Api
                 .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri(configuration.GetValue<string>("ElastiSearch")))
                 {
                     CustomFormatter = new ExceptionAsObjectJsonFormatter(renderMessage: true),
-
+                  
                     AutoRegisterTemplate = true,
                     IndexFormat = $"{Assembly.GetExecutingAssembly().GetName().Name.ToLower().Replace(".", "-")}-{environment?.ToLower().Replace(".", "-")}-{DateTime.UtcNow:yyyy-MM}"
                 })
@@ -57,12 +57,8 @@ namespace ElasticSearchWeb.Api
                   .ConfigureWebHostDefaults(webBuilder =>
                   {
                       webBuilder.UseStartup<Startup>();
-                  }).ConfigureAppConfiguration(configuration =>
-                  {
-                      configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
-                      configuration.AddJsonFile(
-                          $"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json", optional: true);
-                  })
-          .UseSerilog();
-    } 
+                  }).UseSerilog();
+          
+    }
 }
+
